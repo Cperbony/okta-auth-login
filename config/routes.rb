98703756b06a362e7c5f4f83c9dd1a8e6433a665
Claudinei Perboni <cperbony@gmail.com>
2021-09-10 15:54:31 -0300

    update controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   get 'pages/home'
   get 'pages/account'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
